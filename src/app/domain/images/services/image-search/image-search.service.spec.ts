728757923b40a4376aedf66c6f164ee7999095d1
Eloy Pérez <ep.falagan@gmail.com>
2025-05-28 19:36:03 +0200

    test: complete image search service test

--- src/app/domain/images/services/image-search/image-search.service.spec.ts
+++ src/app/domain/images/services/image-search/image-search.service.spec.ts
@@ -1,7 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
 import { ImageSearchService } from './image-search.service';
 import { UnsplashRepository } from '../../../../infra/repositories/unplash/unsplash.repository';
-
+import { ImageSearch } from '../../models/image-search.model';
+import { ImageList } from '../../models/image-list.model';
+import { UnsplashSearchParams } from '../../../../infra/repositories/unplash/models/unsplash-searh-params';
+import { UnsplashApiResponsePaginated } from '../../../../infra/repositories/unplash/models/unsplash-api-response';
+import { UnsplashImage } from '../../../../infra/repositories/unplash/models/unsplash-image';
+import { UnsplashApiResponseBuilder } from '../../../../infra/repositories/unplash/testing/unsplash-api-response-mock.builder';
 
 describe('ImageSearchService', () => {
   let service: ImageSearchService;
@@ -22,5 +28,34 @@ describe('ImageSearchService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the correct ImageList model', (done) => {
+    // Arrange
+    const searchMockInput: ImageSearch = {
+      text: 'cats',
+      orderBy: 'latest',
+      itemsPerPage: 5,
+      page: 1,
+    };
+    const mockApiResponse = new UnsplashApiResponseBuilder()
+      .withTotal(100)
+      .withResults([])
+      .build();
+    // Act
+    repositoryMock.getImages.and.returnValue(of(mockApiResponse));
+    // Assert
+    service.getImages(searchMockInput).subscribe((imageList: ImageList) => {
+      expect(repositoryMock.getImages).toHaveBeenCalledWith({
+        text: 'cats',
+        order_by: 'latest',
+        per_page: 5,
+        page: 1,
+      } as UnsplashSearchParams);
+
+      expect(imageList.total).toBe(100);
+      expect(imageList.items.length).toBe(0);
+      done();
+    });
   });
 });
