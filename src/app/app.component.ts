57b47a262ab1d875a73701db3963675a754ac868
Eloy Pérez <ep.falagan@gmail.com>
2025-05-28 12:05:38 +0200

    feat: clean app root files and define layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<router-outlet />`,
+  standalone: true
 })
-export class AppComponent {
-  title = 'pickmage';
-}
+export class AppComponent {}
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-layout',
+  imports: [HeaderComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  standalone: true,
+})
+export class LayoutComponent {}
