a1356e8c078f3a4a8bbe0392c62aad6992ea0306
Eloy Pérez <ep.falagan@gmail.com>
2025-05-29 00:54:20 +0200

    feat: implement images search in feature image search view

--- src/app/features/image-search/image-search.component.ts
+++ src/app/features/image-search/image-search.component.ts
@@ -1,14 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ImageCardComponent } from '../../domain/images/components/image-card/image-card.component';
-import { ImageSearchService } from '../../domain/images/services/image-search/image-search.service';
+import { ImageSearchService } from './services/image-search/image-search.service';
 import { ImageList } from '../../domain/images/models/image-list.model';
 import { Observable } from 'rxjs';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { ImageGalleryComponent } from '../../shared/image-gallery/image-gallery.component';
+import { InputSearchComponent } from '../../shared/input-search/input-search.component';
 
 @Component({
   selector: 'app-image-search',
-  imports: [NgFor, NgIf, AsyncPipe, ImageCardComponent, ImageGalleryComponent],
+  imports: [
+    NgFor,
+    NgIf,
+    AsyncPipe,
+    ImageCardComponent,
+    ImageGalleryComponent,
+    InputSearchComponent,
+  ],
   templateUrl: './image-search.component.html',
   styleUrl: './image-search.component.scss',
   providers: [ImageSearchService],
@@ -17,8 +25,9 @@ import { ImageGalleryComponent } from '../../shared/image-gallery/image-gallery.
 })
 export class ImageSearchComponent {
   private readonly imageSerarchService = inject(ImageSearchService);
-  public $images: Observable<ImageList> = this.imageSerarchService.getImages({
-    page: 1,
-    itemsPerPage: 10,
-  });
+  public $images = this.imageSerarchService.$images;
+  
+  onSearch(text: any) {
+    this.imageSerarchService.getImages({ page: 1, itemsPerPage: 10, text })
+  }
 }
