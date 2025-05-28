3db6bbba940b08624a57c8a2351f2b97998e5c0d
Eloy Pérez <ep.falagan@gmail.com>
2025-05-28 22:52:57 +0200

    chore: setup unsplash api key

--- src/app/infra/repositories/unplash/config/unsplash-api.config.ts
+++ src/app/infra/repositories/unplash/config/unsplash-api.config.ts
@@ -1,6 +1,6 @@
 export const UNSPLASH_API = {
   BASE_URL: 'https://api.unsplash.com',
-  API_KEY: '',
+  API_KEY: '3ZS_5fsrZ2S5ClhnybHPujWFavpP8n4_bqU7wdz1gfg',
   ROUTES: {
     SEARCH_PHOTOS: 'search/photos',
   },
